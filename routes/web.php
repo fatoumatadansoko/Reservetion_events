17a28af5d58241b686875cac2bda7a274cccbb28
Amadou Barro <barroama23@gmail.com>
2024-07-04 15:04:39 +0000

    reglage de l'intégration instable avec l'ajout des layout sur profil utilisateur

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,6 @@ Route::middleware('utilisateur')->group(function () {
 });
 
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
